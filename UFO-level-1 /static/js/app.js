886938198e18f9b14561afc03404dc821c7dd338
angiecruz <angiecruzalmanza@gmail.com>
2021-06-25 23:15:37 -0500

    looping through data and console.log rows

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -1,5 +1,10 @@
 // from data.js
 var tableData = data;
 
-console.log(data);
+// We can add a console.log(data); to explore our data. 
+// console.log(data); 
+
 // YOUR CODE HERE!
+
+// 1.- Looping through my data and console.log each row. 
+data.forEach(element => console.log(element)); 
