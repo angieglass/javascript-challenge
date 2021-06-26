eb5fe150b430098d3e514dd7c1872f8bbbed7cbe
angiecruz <angiecruzalmanza@gmail.com>
2021-06-25 23:20:15 -0500

    Added the rows

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -1,10 +1,19 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
 // We can add a console.log(data); to explore our data. 
 // console.log(data); 
 
 // YOUR CODE HERE!
 
 // 1.- Looping through my data and console.log each row. 
-data.forEach(element => console.log(element)); 
+tableData.forEach(element => console.log(element)); 
+
+// 2.- Appending a row for each object in my UFO data 
+
+tableData.forEach((element) => {
+    var row = tbody.append("tr")
+})
