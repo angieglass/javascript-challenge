3cccc0d410e9534cb610a13eaf485048a6e01fd5
angiecruz <angiecruzalmanza@gmail.com>
2021-06-26 18:32:09 -0500

    working on the datetime function. Order is correct now

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -5,12 +5,12 @@ var tbody = d3.select("tbody");
 
 // We can add a console.log(data); to explore our data. 
 // console.log(data); 
-
+// ----------------------------------------------------
 // Homework code starts here!
-
+// ----------------------------------------------------
 // 1.- Looping through my data and console.log each row. 
 // tableData.forEach(element => console.log(element)); 
-
+// ----------------------------------------------------
 // 2.- For each element in the array, we add a table row. 
 // Then we add the value on a cell with "value"
 
@@ -21,13 +21,30 @@ tableData.forEach((element) => {
     })
 });
 
-// Event listener for the filter table 
 
+
+// ----------------------------------------------------
+// Select the filter button
 var ftrTableBtn = d3.select("#filter-btn");
-function filterTableButton(){
-    console.log("Filter table clicked")
-    console.log(d3.event.target);
-}
 
-ftrTableBtn.on("click", filterTableButton);
+// Select the forms 
+var datetimeForm = d3.select("#datetime");
+var cityForm = d3.select("#city");
+var stateForm = d3.select("#state");
+var countryForm = d3.select("#country");
+var shapeForm = d3.select("#shape");
+
+// Create event handlers
+ftrTableBtn.on("click", runFilter);
+datetimeForm.on("submit", runFilter);
+
+
+// ----------------------------------------------------
+//Event handler function to filter data 
+function runFilter(){
+    d3.event.preventDefault();
+    var inputDatetime = datetimeForm.property("value"); 
+console.log(inputDatetime);
+console.log(tableData);
+}
 
